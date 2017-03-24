381efe86029742718f5d901fcc1a6725253eab6d
pirminkager <pirmink@gmail.com>
2017-03-24 19:29:34 +0100

    Alpha Version! (almost finished, testing)

--- solarboilerswitch/solarboilerswitch.ino
+++ solarboilerswitch/solarboilerswitch.ino
@@ -1,6 +1,16 @@
 #include <LiquidCrystal.h>
 #define NUM_TIMER 10
 
+// Functionparameters
+#define A_B 10
+#define B_B 20
+#define C_B 30
+#define D_B 40
+#define A_S 10
+#define B_S 10
+#define C_S 20
+#define D_S 12
+
 LiquidCrystal lcd(12,11,7,8,9,10);
 #include "customsymbols.h"
 
@@ -34,12 +44,13 @@ volatile boolean sourceSelect=false;
 volatile boolean modeSelect=false;
 boolean stateS;
 boolean stateB;
-boolean previousvalveState=false;
+boolean previousvalveState;
+boolean undefinedvalveState=false;
 char *mode[] = {"A","M"};
-char buff[5];
+char buff[4];
 long timerValue[NUM_TIMER];
-const double calibrationSolar[4]={1,2,3,4};
-const double calibrationBoiler[4]={1,2,3,4};
+const double calibrationSolar[4]={A_S,B_S,C_S,D_S};
+const double calibrationBoiler[4]={A_B,B_B,C_B,D_B};
 
 void getsensorData() {
   rawsolarValue=rawsolarValue+analogRead(tempsolarPin);
@@ -51,10 +62,13 @@ void getsensorData() {
     nReads=0;
     tempBoiler=calcfloattemp(tempboilerValue, calibrationBoiler);
     tempSolar=calcfloattemp(tempsolarValue, calibrationSolar);
-    sprintf(buff,"[%2d.%.1d]\n", (int) (tempBoiler/10), (int) (tempBoiler%10));
-    lcdsendString(0,10,4,buff);
-    sprintf(buff,"[%2d.%.1d]\n", (int) (tempSolar/10), (int) (tempSolar%10));
-    lcdsendString(1,10,4,buff);
+    sprintf(buff,"%2d.%.1d", (int) (tempBoiler/10), (int) (tempBoiler%10));
+    //lcdsendString(0,10,4,buff);
+    lcd.setCursor(0,10);
+    lcd.write(buff,4);
+    sprintf(buff,"%2d.%.1d", (int) (tempSolar/10), (int) (tempSolar%10));
+    lcd.setCursor(1,10);
+    lcd.write(buff,4);
   }
 }
 
@@ -82,6 +96,14 @@ void modeSelection() {
     else {
       modeSelect=false;
     }
+    if (modeSelect==true) {
+      lcd.setCursor(0,0);
+      lcd.write('A');
+    }
+    else if (modeSelect==false) {
+      lcd.setCursor(0,0);
+      lcd.write('M');
+    }
     deBounce(modeButton);
     EIFR = bit (INTF1);
     modebuttonFlag=false;
@@ -98,10 +120,27 @@ void sourceSelection() {
     else {
       sourceSelect=false;
     }
+    if (sourceSelect==true && modeSelect == false) {
+      digitalWrite(togglevalvePin, true);
+    }
+    else if (sourceSelect==false && modeSelect == false) {
+      digitalWrite(togglevalvePin, false);
+    }
     deBounce(sourceButton);
     EIFR = bit (INTF0);
     sourcebuttonFlag=false;
     attachInterrupt (digitalPinToInterrupt (sourceButton), sourceSelection, FALLING);
+  }
+}
+
+void automaticMode() {
+  if (modeSelect == true) {
+    if (tempBoiler - 50 > tempSolar) {
+      digitalWrite(togglevalvePin, true);
+    }
+    else if (tempSolar - 50 > tempBoiler) {
+      digitalWrite(togglevalvePin, false);
+    }
   }
 }
 
@@ -132,22 +171,30 @@ void lcdcustomSend (int col, int row, int character ) {
 void checkvalvestate() {
   stateS=digitalRead(valvestatePin1);
   stateB=digitalRead(valvestatePin2);
+  if (stateS) {
+    previousvalveState=0;
+    lcdcustomSend (previousvalveState,2,0);
+  }
+  if (stateB) {
+    previousvalveState=1;
+    lcdcustomSend (previousvalveState,2,0);
+  }
 }
 
 void animation() {
   if (!stateS && !stateB) {
-    if (!previousvalveState) {
-      previousvalveState=true;
+    if (!undefinedvalveState) {
+      undefinedvalveState=true;
     }
-    lcdcustomSend (0,1,animNumber);
+    lcdcustomSend (digitalRead(togglevalvePin),2,animNumber);
     animNumber++;
     if (animNumber == 3) {
       animNumber = 1;
     }
   }
   else if (previousvalveState && ( stateS || stateB )) {
-    lcdcustomSend (0,1,5);
-    previousvalveState=false;
+    lcdcustomSend (0,2,5);
+    undefinedvalveState=false;
   }
 }
 
@@ -173,7 +220,16 @@ void setup() {
   lcd.createChar(4, anim4);
   lcd.createChar(5, empty);
   lcd.begin(16,2);
-  lcd.print("SOLAR TEMP");
+  lcd.setCursor(0,3);
+  lcd.write("Solar");
+  lcd.setCursor(1,3);
+  lcd.write("Boiler");
+  lcd.setCursor(0,14);
+  lcd.print(char(223));
+  lcd.write('C');
+  lcd.setCursor(1,14);
+  lcd.print(char(223));
+  lcd.write('C');
   
   pinMode(togglevalvePin, OUTPUT);
   pinMode(valvestatePin1, INPUT);
@@ -191,15 +247,17 @@ void setup() {
 void loop() {
   
   //readSensor and Calculate Temperature.
-  if (timer(10000,0)) {
-    
+  if (timer(1000,0)) {
+    getsensorData();
   }
   
   //check Valvestate and Animation
-  if (timer(2000,1)) {
+  if (timer(200,1)) {
     checkvalvestate();
     animation();
+    modeSelection();
+    sourceSelection();
+    automaticMode();
   }
-  
   
 }
